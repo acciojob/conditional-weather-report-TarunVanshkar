807ba46a569d901a8b11fda442e9a51ae64b657b
TarunVanshkar <tarunben33@gmail.com>
2023-07-15 21:33:04 +0530

    submitting code-Sat Jul 15 2023 21:32:46 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [weather, setWeather] = useState({ temperature: 25, conditions: "Sunny" })
+  const [temp, setTemp] = useState(0);
+
+  const temperatureColor = weather.temperature > 20 ? 'red' : 'blue';
+  function handleClick() {
+    setWeather({ ...weather, temperature: temp, conditions: "Sunny" })
+    console.log(weather)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Weather Report</h1>
+      <input type="text" placeholder="Enter Temperature" onChange={(e) => setTemp(e.target.value)} />
+      <button onClick={handleClick}>Show Condition</button>
+      <p>Temperature : <span style={{ color:temperatureColor }}>{weather.temperature}</span></p>
+      <p>Condition : weather.conditions</p>
     </div>
   )
 }
